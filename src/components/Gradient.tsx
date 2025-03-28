67807afd7eb2f01d3b11ff5b97641fa7e3f9ee6f
Alan Casal <alan.casal.dev@gmail.com>
2025-03-28 16:17:02 -0300

    Added animation for radial gradient

--- src/components/Gradient.tsx
+++ src/components/Gradient.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import {
 	Canvas,
@@ -6,10 +6,22 @@ import {
 	RadialGradient,
 	vec,
 } from '@shopify/react-native-skia';
+import { useSharedValue, withTiming } from 'react-native-reanimated';
 
 const Gradient = () => {
 	const [width, setWidth] = useState(0);
 	const [height, setHeight] = useState(0);
+
+	const gradient = useSharedValue(0);
+
+	useEffect(() => {
+		if (width !== 0) {
+			gradient.value = withTiming(width / 2, {
+				duration: 1000,
+			});
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [width]);
 
 	return (
 		<Canvas
@@ -27,7 +39,7 @@ const Gradient = () => {
 				<RadialGradient
 					colors={['#242B4D', 'black']}
 					c={vec(width / 2, height / 2)}
-					r={width / 2}
+					r={gradient}
 				/>
 			</Circle>
 		</Canvas>
