30fecb2ffff92308dd1a58d4cc580dd4999f7307
Alan Casal <alan.casal.dev@gmail.com>
2025-03-28 09:28:27 -0300

    Added dependencies, babel and metro config

---
+++ metro.config.js
@@ -0,0 +1,16 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+module.exports = (() => {
+	const config = getDefaultConfig(__dirname);
+
+	const { resolver } = config;
+
+	config.resolver = {
+		...resolver,
+		assetExts: [...resolver.assetExts, 'glb', 'gltf', 'png', 'jpg'],
+		sourceExts: [...resolver.sourceExts, 'js', 'jsx', 'json', 'ts', 'tsx', 'cjs', 'mjs'],
+	};
+
+	return config;
+})();
