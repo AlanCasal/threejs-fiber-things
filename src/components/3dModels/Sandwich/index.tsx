cd78512fdf8dd0a034862320b458e45dfe1d3a98
Alan Casal <alan.casal.dev@gmail.com>
2025-04-01 00:32:11 -0300

    added rotation animations

--- src/components/3dModels/Sandwich/index.tsx
+++ src/components/3dModels/Sandwich/index.tsx
@@ -1,18 +1,32 @@
 /* eslint-disable react/no-unknown-property */
-import { useSandwich } from 'src/hooks/useSandwich';
+import { useRef } from 'react';
+import { Group } from 'three';
+import { useFrame } from '@react-three/fiber/native';
 import Ingredient from './Ingredient';
+import { useSandwich } from 'src/hooks/useSandwich';
 
 const INGREDIENT_SPACING = 0.2;
 const INGREDIENT_SPACING_FINAL = 0.06;
 
 const Sandwich = () => {
+	const sandwichRef = useRef<Group>(null);
+
 	const { ingredients, addedToCart } = useSandwich();
 	const ingredientSpacing = addedToCart
 		? INGREDIENT_SPACING_FINAL
 		: INGREDIENT_SPACING;
 
+	useFrame(() => {
+		addedToCart
+			? (sandwichRef.current!.rotation.y += 0.01)
+			: (sandwichRef.current!.rotation.y = 0);
+	});
+
 	return (
-		<group position-y={(-ingredients.length * ingredientSpacing) / 2}>
+		<group
+			position-y={(-ingredients.length * ingredientSpacing) / 2}
+			ref={sandwichRef}
+		>
 			{ingredients.map((ingredient, index) => (
 				<Ingredient
 					key={ingredient.id + ingredient.name}
