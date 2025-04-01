6aa0b2d15383041fc9db96cd086d7ac603793b3e
Alan Casal <alan.casal.dev@gmail.com>
2025-04-01 00:09:39 -0300

    added dynamic ingredient spacing

--- src/components/3dModels/Sandwich/index.tsx
+++ src/components/3dModels/Sandwich/index.tsx
@@ -1,19 +1,24 @@
+/* eslint-disable react/no-unknown-property */
 import { useSandwich } from 'src/hooks/useSandwich';
 import Ingredient from './Ingredient';
 
 const INGREDIENT_SPACING = 0.2;
+const INGREDIENT_SPACING_FINAL = 0.06;
 
 const Sandwich = () => {
-	const { ingredients } = useSandwich();
+	const { ingredients, addedToCart } = useSandwich();
+	const ingredientSpacing = addedToCart
+		? INGREDIENT_SPACING_FINAL
+		: INGREDIENT_SPACING;
 
 	return (
-		<group>
+		<group position-y={(-ingredients.length * ingredientSpacing) / 2}>
 			{ingredients.map((ingredient, index) => (
 				<Ingredient
 					key={ingredient.id + ingredient.name}
 					showPrice={index > 0 && index < ingredients.length - 1}
 					ingredient={ingredient}
-					position-y={index * INGREDIENT_SPACING}
+					position-y={index * ingredientSpacing}
 				/>
 			))}
 		</group>
