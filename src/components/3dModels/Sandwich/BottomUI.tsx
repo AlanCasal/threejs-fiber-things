e9a677a29a14e3a9bb3f657746619d9922e24c4e
Alan Casal <alan.casal.dev@gmail.com>
2025-03-31 22:44:24 -0300

    Added styling

--- src/components/3dModels/Sandwich/BottomUI.tsx
+++ src/components/3dModels/Sandwich/BottomUI.tsx
@@ -1,17 +1,27 @@
-import { Text, SafeAreaView } from 'react-native';
-import Button from '../../common/Button';
+import { SafeAreaView, View, Text } from 'react-native';
 
 const BottomUI = () => {
 	return (
-		<SafeAreaView className='p-5'>
-			<Text className='text-black text-2xl font-bold text-center mb-10'>
-				Food 🍔
-			</Text>
-			<Button
-				handlePress={() => {}}
-				text='Add Food'
-				className='bg-primary-500'
-			/>
+		<SafeAreaView>
+			<View className='p-5'>
+				<View className='flex-row gap-2 items-center'>
+					<Text className='flex-shrink text-xl font-extrabold'>
+						Armate tu Chegusán ! 🥪
+					</Text>
+					<Text>⭐⭐⭐⭐⭐</Text>
+				</View>
+
+				<Text className='text-primary-400'>
+					Fresh and delicious sandwiches made with love
+				</Text>
+				<View className='h-[1px] bg-light-200 my-5' />
+				<Text className='text-lg font-extrabold text-center'>
+					Your Creation ($5.00)
+				</Text>
+				<Text className='text-center text-dark-200 font-bold'>
+					Compose your sandwich by adding ingredients
+				</Text>
+			</View>
 		</SafeAreaView>
 	);
 };
